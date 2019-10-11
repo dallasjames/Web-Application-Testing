87f38f6c6908148162e7509e4f600cefcaa232b8
Dallas James <dallasjames42@gmail.com>
2019-10-10 22:43:18 -0600

    testing done

--- web-app-testing/src/App.test.js
+++ web-app-testing/src/App.test.js
@@ -1,7 +1,6 @@
 import React from "react";
 import * as rtl from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
-import Display from "./Display";
 import App from './App';
 
 afterEach(rtl.cleanup);
